cbdbee9a3617dafe93aef6b1540054d709d9d2c1
lnewson <lnewson@redhat.com>
2012-12-03 17:15:57 +1000

    Added fixes to follow specifications about response body data for certain status codes.

--- src/main/java/org/jboss/pressgang/ccms/restserver/servlet/filter/GZIPServletOutputStream.java
+++ src/main/java/org/jboss/pressgang/ccms/restserver/servlet/filter/GZIPServletOutputStream.java
@@ -8,13 +8,26 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * A wrapper class to allow using a {@link java.util.zip.GZIPOutputStream GZIPOutputStream} as a {@link javax.servlet.ServletOutputStream ServletOutputStream}.
- * <br /><br />
- * See: <a href="http://stackoverflow.com/questions/4755302/which-compression-is-gzip-the-most-popular-servlet-filter-would-you-suggest/11068672#11068672">which-compression-is-gzip-the-most-popular-servlet-filter-would-you-suggest</a>
- * and <a href="https://github.com/geoserver/geoserver/tree/master/src/main/src/main/java/org/geoserver/filters">https://github.com/geoserver/geoserver/</a>
- * for details on where this implementation came from.
+ * A wrapper class to allow using a {@link java.util.zip.GZIPOutputStream GZIPOutputStream} as a
+ * {@link javax.servlet.ServletOutputStream ServletOutputStream}. <br />
+ * <br />
+ * See: <a href=
+ * "http://stackoverflow.com/questions/4755302/which-compression-is-gzip-the-most-popular-servlet-filter-would-you-suggest/11068672#11068672"
+ * >which-compression-is-gzip-the-most-popular-servlet-filter-would-you-suggest</a> and <a
+ * href="https://github.com/geoserver/geoserver/tree/master/src/main/src/main/java/org/geoserver/filters"
+ * >https://github.com/geoserver/geoserver/</a> for details on where this implementation came from.
  */
 public class GZIPServletOutputStream extends ServletOutputStream {
+    /**
+     * Gzipping an empty file or stream always results in a 20 byte output This is in java or elsewhere.
+     * <p/>
+     * On a linux system to reproduce do <code>gzip -n empty_file</code>. -n tells gzip to not include the file name. The
+     * resulting file size is 20 bytes.
+     * <p/>
+     * Therefore 20 bytes can be used indicate that the gzip byte[] will be empty when ungzipped.
+     */
+    private static final int EMPTY_GZIPPED_CONTENT_SIZE = 20;
+
     protected ByteArrayOutputStream baos = null;
     protected GZIPOutputStream GZIPStream = null;
     protected boolean closed = false;
@@ -37,17 +50,29 @@ public class GZIPServletOutputStream extends ServletOutputStream {
         }
         GZIPStream.finish();
 
+        int status = response.getStatus();
         final byte[] bytes = baos.toByteArray();
-        final String contentLength = Integer.toString(bytes.length);
         
-        if (response.containsHeader("Content-Length")) {
-            response.setHeader("Content-Length", contentLength);
+        // Check if the response should be empty based on the response code
+        // Also only set the gzip header and content when the response isn't empty.
+        if (!shouldBodyBeZero(status) && !isBodyEmpty(bytes)) {
+            final String contentLength = Integer.toString(bytes.length);
+
+            if (response.containsHeader("Content-Length")) {
+                response.setHeader("Content-Length", contentLength);
+            } else {
+                response.addHeader("Content-Length", contentLength);
+            }
+
+            response.addHeader("Content-Encoding", "gzip");
+            output.write(bytes);
         } else {
-            response.addHeader("Content-Length", contentLength);
+            if (response.containsHeader("Content-Length")) {
+                response.setIntHeader("Content-Length", 0);
+            } else {
+                response.addIntHeader("Content-Length", 0);
+            }
         }
-         
-        response.addHeader("Content-Encoding", "gzip");
-        output.write(bytes);
         output.flush();
         output.close();
         closed = true;
@@ -65,7 +90,7 @@ public class GZIPServletOutputStream extends ServletOutputStream {
         if (closed) {
             throw new IOException("Cannot write to a closed output stream");
         }
-        GZIPStream.write((byte)b);
+        GZIPStream.write((byte) b);
     }
 
     public void write(byte b[]) throws IOException {
@@ -84,5 +109,27 @@ public class GZIPServletOutputStream extends ServletOutputStream {
     }
 
     public void reset() {
+    }
+
+    protected boolean isBodyEmpty(byte[] compressedOutput) {
+        if (compressedOutput.length == EMPTY_GZIPPED_CONTENT_SIZE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    protected boolean shouldBodyBeZero(int responseStatus) {
+        // Check for NO_CONTENT
+        if (responseStatus == HttpServletResponse.SC_NO_CONTENT) {
+            return true;
+        }
+
+        // Check for NOT_MODIFIED
+        if (responseStatus == HttpServletResponse.SC_NOT_MODIFIED) {
+            return true;
+        }
+
+        return false;
     }
 }
